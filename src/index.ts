f1f0cec4088615d83426ccdd9d5aa19fd9cc96b0
re-up-hq <hq@re-up.ph>
2024-09-17 09:26:41 +0800

    rm: Element

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 /* fn accepts 2 nullable elements. useful for conditional rendering.  */
-const condish = (
-  ...args: (Element | null | string | number | boolean | undefined)[]
-) =>
+const condish = (...args: (null | string | number | boolean | undefined)[]) =>
   new Map([
     [true, args[0]],
     [false, args[1]],
